01fb3f9c4be75ef84afe6ae75456516a242cd0e8
Dasu <v.dasu@dxc.com>
2023-04-20 12:20:54 +0530

    task updated

--- csharp/Check wheather character is Vowel or Not/Check wheather character is Vowel or Not/Program.cs
+++ csharp/Check wheather character is Vowel or Not/Check wheather character is Vowel or Not/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Enter any Character");
 char c=Convert.ToChar(Console.ReadLine());
-
-if(c == 'a' || c== 'e' || c== 'i' || c== 'o' || c== 'u')
+c = char.ToLower(c);
+if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
 {
     Console.WriteLine(c + " is a vowel");
 }
