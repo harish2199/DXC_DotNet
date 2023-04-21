5a72b416ccaf254dca3ffdc9c5b1548e3d2d6dcd
Dasu <v.dasu@dxc.com>
2023-04-21 11:33:34 +0530

    Day3

--- csharp/Day3_Task3/Day3_Task3/Program.cs
+++ csharp/Day3_Task3/Day3_Task3/Program.cs
@@ -1,8 +1,9 @@
-﻿Console.WriteLine("Temperature(in celsius) \t Temperature(in Fahrenheit)");
+﻿int C = 0;
+Console.WriteLine("Sl.No \t Temperature(in celsius) \t Temperature(in Fahrenheit)");
 for (int i = 1; i <= 10; i++)
 {
-    double C = double.Parse(Console.ReadLine());
     double F = (C * 9 / 5) + 32;
-    Console.Write("\t\t\t\t" + F);
+    Console.Write(i+"\t"+C+"\t\t\t\t" + F);
     Console.WriteLine();
+    C = C + 10;
 }
