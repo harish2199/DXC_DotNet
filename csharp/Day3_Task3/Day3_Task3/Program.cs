2b0cc492d56e716539e13937c0fd6c9b37b3baa1
Dasu <v.dasu@dxc.com>
2023-04-21 10:45:07 +0530

    Day3

---
+++ csharp/Day3_Task3/Day3_Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Enter the temperature in Degree Celsius");
+    double C = double.Parse(Console.ReadLine());
+    double F = (C * 9 / 5) + 32;
+    Console.WriteLine($"Temperature(in celsius) {C} is Equivalent to Temperature(in Fahrenheit) {F}");
+}
